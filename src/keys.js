6ee8831660afcca500af1afa19b4639e054c0c0b
Samir Saldarriaga <s4m1r.5a@gmail.com>
2019-09-18 18:37:07 -0500

    nueva  base de datos

--- src/keys.js
+++ src/keys.js
@@ -1,11 +1,11 @@
 module.exports = {
 
     database: {
-        connectionLimit: 55,
-        host: 'remotemysql.com',
-        user: '7FkgiibGMr',
-        password: 'h8cC0kkPPE',
-        database: '7FkgiibGMr'
+        connectionLimit: 10,
+        host: '213.190.6.64',
+        user: 'u152781536_s',
+        password: 'cinetflix',
+        database: 'u152781536_redf'
     }
 
 };
